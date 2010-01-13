adabc6f198124de934e5464a0e9f0faf4db10f40
Brandon Dimcheff <bdimchef-git@wieldim.com>
2010-01-13 09:08:25 -0800

    multiple role support (stolen from railscasts)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,26 @@
 class User < ActiveRecord::Base
   acts_as_authentic
   
+  ROLES = %w[attendee speaker organizer]
+  
   def has_role?(role)
-    self.role == role
+    roles.include? role
+  end
+  
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.sum
+  end
+  
+  def roles
+    ROLES.reject { |r| ((roles_mask || 0) & 2**ROLES.index(r)).zero? }
+  end
+  
+  def has_awesome?
+    true
+  end
+  
+  def role_symbols
+    roles.map(&:to_sym)
   end
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,43 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @user = Factory.create(:user, :role => 'some_role')
-  end
-
-  it 'can assign one role' do
-    @user.role.should == 'some_role'
+    @user = Factory.create(:user)
   end
   
-  it 'determines if the user has a role' do
-    @user.has_role?('some_role').should be_true
+  describe "with no role" do
+    it 'should have no roles' do
+      @user.roles.should == []
+    end
+  end
+  
+  describe "with a single role" do
+    before(:each) do
+      @user.roles = ['attendee']
+    end
+
+    it 'can assign one role' do
+      @user.roles.should == ['attendee']
+    end
+
+    it 'determines if the user has a role' do
+      @user.has_role?('attendee').should be_true
+    end
+  end
+  
+  describe 'with multiple roles' do
+    before(:each) do
+      @user.roles = ['attendee', 'speaker']
+    end
+    
+    it 'can assign multiple roles' do
+      @user.roles.should have(2).roles
+      @user.roles.should include('attendee')
+      @user.roles.should include('speaker')
+    end
+    
+    it 'determines if the user has the role' do
+      @user.has_role?('attendee').should be_true
+      @user.has_role?('speaker').should be_true
+    end
   end
 end
